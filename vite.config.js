7073877152036ed54d869aee7dc354fff9f28bf5
IndrajitParmar <parindra231@gmail.com>
2023-06-29 13:46:35 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/IndrajitParmar/real-state.github.io/",
+  base: "/real-state.github.io/",
   plugins: [react()],
 });
